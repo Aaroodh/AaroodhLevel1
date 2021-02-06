80ce94df124d0921af21864ac8edd50785deb22f
Aaroodh <72703945+Aaroodh@users.noreply.github.com>
2021-02-06 14:20:28 +0530

    Update tromboloid_with_4_functions.c

--- tromboloid_with_4_functions.c
+++ tromboloid_with_4_functions.c
@@ -1,1 +1,39 @@
 //WAP to find the volume of a tromboloid using 4 functions.
+
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+float vol(float h,float b,float d)
+{   float volume;  
+   volume=((h*d*b)+d/b)/3.0;
+   return volume;
+}
+
+float inp()
+{ float h,b,d,o;
+    printf("enter h ,b ,d \n");
+    scanf("%f %f %f",&h,&b,&d);
+    o=vol(h,b,d);
+    return o;
+   
+}
+
+void disp(float m)
+{
+    printf("volume=%f",m);
+}
+
+
+
+void main()
+{ float m,n;
+    m=inp();
+    disp(m);
+}
