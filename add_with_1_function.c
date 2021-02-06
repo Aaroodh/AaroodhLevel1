213c6b3a6fbd091149dae94e60efc7b0adeecc0a
Aaroodh <72703945+Aaroodh@users.noreply.github.com>
2021-02-06 13:04:49 +0530

    Update add_with_1_function.c

--- add_with_1_function.c
+++ add_with_1_function.c
@@ -1,1 +1,12 @@
+
+
+#include <stdio.h>
+
+int main()
+{ char a[20];
+    printf("enter the name /n");
+    scanf("%s",&a);
+    printf("hello %s /n",a);
+    return 0;
+}
 //Write a program to add two user input numbers using one function.
